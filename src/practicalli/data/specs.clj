f82840d2f9cac9717c794210e14dd3af893a019a
Dmitry Pavluk <dmitry@canopyservicing.com>
2020-11-11 06:02:07 -0500

    define data models for charge, payment, interest, fee

--- src/practicalli/data/specs.clj
+++ src/practicalli/data/specs.clj
@@ -6,7 +6,6 @@
 
 
 ;; Customer detail specifications
-
 (spec/def :customer/id uuid?)
 (spec/def :customer/legal-name string?)
 (spec/def :customer/email-address string?)
@@ -17,23 +16,57 @@
 ;; Data to send to the database
 (spec/def :customer/unregistered
   (spec/keys
-    :req [:customer/legal-name
-          :customer/email-address
-          :customer/residential-address
-          :customer/social-security-number]
-    :opt [:customer/preferred-name]))
+   :req [:customer/legal-name
+         :customer/email-address
+         :customer/residential-address
+         :customer/social-security-number]
+   :opt [:customer/preferred-name]))
 
 
 ;; Data received from the database
 (spec/def :customer/registered
   (spec/keys
-    :req [:customer/id
-          :customer/legal-name
-          :customer/email-address
-          :customer/residential-address
-          :customer/social-security-number]
-    :opt [:customer/preferred-name]))
+   :req [:customer/id
+         :customer/legal-name
+         :customer/email-address
+         :customer/residential-address
+         :customer/social-security-number]
+   :opt [:customer/preferred-name]))
 
+
+;; Define attributes that are common to a line item
+(spec/def :line-item/id uuid?)
+(spec/def :line-item/account-id uuid?)
+(spec/def :line-item/product-id uuid?)
+(spec/def :line-item/interest-rate double?)
+(spec/def :line-item/amount-cents double?)
+(spec/def :line-item/merchant-info string?)
+(spec/def :line-item/description string?)
+
+;; NOTE: 
+;; I dont think we need these. When we query data, we will know
+;; it's a charge because of the entity type, and payment and schedule orders
+;; can be defined in the application layer.
+;; 
+;; (spec/def :charge/type "CHARGE")
+;; (spec/def :charge/payment-order 3)
+;; (spec/def :charge/schedule-order 3)
+
+(spec/def :line-item/common
+  (spec/keys
+   :req [:line-item/id
+         :line-item/account-id
+         :line-item/product-id
+         :line-item/interest-rate
+         :line-item/amount-cents]
+   :opt [:line-item/merchant-info :line-item/description]))
+
+;; NOTE: when we need to customize, extend the line-item interface via merge:
+;; https://clojure.org/guides/spec#_entity_maps                        
+(spec/def :line-item/charge :line-item/common)
+(spec/def :line-item/payment :line-item/common)
+(spec/def :line-item/interest :line-item/common)
+(spec/def :line-item/fee :line-item/common)
 
 
 
@@ -58,44 +91,4 @@
   ;; => false
 
   (spec/valid? :customer/id
-               (:id #:customer{:id #uuid "323ecc53-d676-4b7a-bea0-a4b3ca075c2c"}))
-
-
-  ) ;; End of rich comment block
-
-
-
-;; I am using the following schema with a H2 database, which generates a uuid.
-;; (def schema-customer
-;;   ["create table if not exists public.customer (
-
-;;      id                     uuid         default random_uuid() not null,
-;;      legal_name             varchar(32)  not null,
-;;      email_address          varchar(32)  not null,
-;;      residential_address    varchar(255) not null,
-;;      social_security_number varchar(32)  not null,
-;;      preferred_name         varchar(32),
-
-;;      constraint customer_pk primary key (id))"])
-;; This uuid value is returned from a call to next.jdbc.sql/insert! which used the following spec to generate the new record.
-;; (spec/def :customer/unregistered
-;;   (spec/keys
-;;     :req [:customer/legal-name
-;;           :customer/email-address
-;;           :customer/residential-address
-;;           :customer/social-security-number]
-;;     :opt [:customer/preferred-name]))
-;; The update! function returns values in the form: #:customer{:id #uuid "323ecc53-d676-4b7a-bea0-a4b3ca075c2c"}
-;; I check the result from insert! against the :customer/id specification:
-;; (spec/def :customer/id uuid?)
-;; Doing so in a unit test, that test is failing suggesting the returned value from insert! is not conforming to the clojure.spec specification.
-;; expected: (spec/valid?
-;;             :customer/id
-;;             (SUT/new-customer
-;;               (spec-gen/generate (spec/gen :customer/unregistered))))
-
-;; actual: (not
-;;           (spec/valid?
-;;             :customer/id
-;;             #:customer{:id #uuid "323ecc53-d676-4b7a-bea0-a4b3ca075c2c"}))
-;; Not sure where I have gone wrong here... any suggestions?
+               (:id #:customer{:id #uuid "323ecc53-d676-4b7a-bea0-a4b3ca075c2c"}))) ;; End of rich comment block
