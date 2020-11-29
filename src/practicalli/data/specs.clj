53ba304fcd0108fb4bff06896e04e2b0b94c27f1
Dmitry Pavluk <dmitry@canopyservicing.com>
2020-11-29 10:39:52 -0500

    add and retrieve sample payment

--- src/practicalli/data/specs.clj
+++ src/practicalli/data/specs.clj
@@ -53,18 +53,13 @@
 (spec/def :line-item/merchant-info string?)
 (spec/def :line-item/description string?)
 
-;; NOTE: 
-;; I dont think we need these. When we query data, we will know
-;; it's a charge because of the entity type, and payment and schedule orders
-;; can be defined in the application layer.
-;; 
-;; 
-
 
 
 (spec/def :charge/type #{:CHARGE})
 ;; (spec/def :charge/payment-order 3)
 ;; (spec/def :charge/schedule-order 3)
+;; 
+(spec/def :payment/type #{:PAYMENT})
 
 (spec/def :line-item/common
   (spec/keys
@@ -75,11 +70,12 @@
          :line-item/amount-cents]
    :opt [:line-item/merchant-info :line-item/description]))
 
-;; NOTE: when we need to customize, extend the line-item interface via merge:
-;; https://clojure.org/guides/spec#_entity_maps                        
-;; (spec/def :line-item/charge :line-item/common)
+
 (spec/def :line-item/charge (spec/merge :line-item/common
                                         (spec/keys :req [:charge/type])))
+
+(spec/def :line-item/payment (spec/merge :line-item/common
+                                         (spec/keys :req [:payment/type])))
 
 (spec/def :line-item/payment :line-item/common)
 (spec/def :line-item/interest :line-item/common)
@@ -107,11 +103,23 @@
        :interest-rate 4.5
        :amount-cents 1800})
 
+    (def payment1
+      {:crux.db/id "d775a8a3-9ca6-4176-8b8c-ddc2fb9458b4"
+       :account-id "8c899527-fe21-490a-ae45-a58488540806"
+       :product-id "f0b06263-73c3-44a8-a261-ec0e419ef089"
+       :interest-rate 0
+       :amount-cents 1500})
+
     (crux/submit-tx crux [[:crux.tx/put charge1]])
+    (crux/submit-tx crux [[:crux.tx/put payment1]])
 
     (crux/q (crux/db crux)
             '{:find [(eql/project ?line-item/charge [*])]
               :where [[?line-item/charge :crux.db/id "24e56a67-0559-4baa-a16e-9fab034cc84c"]]})
+
+    (crux/q (crux/db crux)
+            '{:find [(eql/project ?line-item/payment [*])]
+              :where [[?line-item/payment :crux.db/id "d775a8a3-9ca6-4176-8b8c-ddc2fb9458b4"]]})
 
 
     (crux/entity-history (crux/db crux) "24e56a67-0559-4baa-a16e-9fab034cc84c" :asc))
